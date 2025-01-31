236f7e4446c69816cc95964df918c7cfe5c26d2e
Romina Rivera <rdrivera@uc.cl>
2025-01-31 01:58:29 -0300

    botones de descargas

--- script.js
+++ script.js
@@ -1,55 +1,36 @@
-// Funciones para manejar eventos
-function showErrorLog() {
-  alert('Mostrando log de errores relacionados con duplicidad de reservas.');
+// Descarga reserva
+const reservasButton = document.getElementById('boton-reservas');
+
+if (reservasButton) {
+  reservasButton.addEventListener('click', () => {
+    downloadReport('reservas');
+  });
+}
+//Descarga informe
+const informeButton = document.getElementById('boton-informe');
+if (informeButton) {
+  informeButton.addEventListener('click', () => {
+    downloadReport('informe');
+  });
+}
+// Descarga stock
+const stockButton = document.getElementById('boton-stock');
+
+if (stockButton) {
+  stockButton.addEventListener('click', () => {
+    downloadReport('stock');
+  });
 }
 
-// contenido de las reservas
-const reservationsData = [
-  { id: 1, room: 'A', status: 'Confirmada' },
-  { id: 2, room: 'B', status: 'Confirmada' },
-];
-
-const paymentsData = [
-  { id: 1, amount: 100, status: 'Procesado', reserveId: 1 },
-  { id: 2, amount: 200, status: 'Procesado', reserveId: 2 },
-];
-
-document.addEventListener('DOMContentLoaded', function () {
-  // Poblar datos dinámicamente
-  /*
-  const paymentsList = document.getElementById('paymentsList');
-  paymentsData.forEach((payment) => {
-    const li = document.createElement('li');
-    li.textContent = `Pago ${payment.id} - Reserva ${payment.reserveId} - $${payment.amount} - Estado: ${payment.status}`;
-    paymentsList.appendChild(li);
-  }); */
-
-  // Navbar
-  const navItems = document.querySelectorAll('.nav-item');
-  navItems.forEach((item) => {
-    item.addEventListener('click', function () {
-      navItems.forEach((el) => el.classList.remove('active'));
-      this.classList.add('active');
-    });
+//Descarga Pagos
+const pagosButton = document.getElementById('boton-pagos');
+if (pagosButton) {
+  pagosButton.addEventListener('click', () => {
+    downloadReport('pagos');
   });
+}
 
-  // Configurar botones de descarga
-  const sincronizacionButton = document.querySelector('#sincronizacion button');
-  if (sincronizacionButton) {
-    sincronizacionButton.addEventListener('click', () => {
-      downloadReport('sincronizacion');
-    });
-  }
-
-  const reservasButton = document.getElementById('boton-reservas');
-
-  if (reservasButton) {
-    reservasButton.addEventListener('click', () => {
-      downloadReport('reservas');
-    });
-  }
-});
-
+// Configurar botones de descarga
 function downloadReport(sectionId) {
   const section = document.getElementById(sectionId);
   if (!section) {
