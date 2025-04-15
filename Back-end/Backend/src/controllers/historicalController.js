48f102a9e28df836363f1f27e1976d419d80ac79
Romina Rivera <rdrivera@uc.cl>
2025-04-15 03:58:04 -0400

    agregar controlador y rutas para gestionar reservas históricas: obtener todas y por ID

---
+++ Back-end/Backend/src/controllers/historicalController.js
@@ -0,0 +1,50 @@
+const HistoricalReservation = require('../models/HistoricalReservation');
+const HistoricalPayment = require('../models/HistoricalPayments');
+
+//Obtener todas las reservas historicas
+exports.getAllHistoricalReservations = async (_, res) => {
+  try {
+    const historicalReservations = await HistoricalReservation.find();
+    res.status(200).json(historicalReservations);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Error al encontrar las reservas historicas' });
+  }
+};
+
+//Obtener una reserva historica por ID
+exports.getHistoricalReservationById = async (req, res) => {
+  try {
+    const historicalReservation = await HistoricalReservation.findById(req.params.id);
+    if (!historicalReservation)
+      return res.status(404).json({ error: 'Reserva historica no encontrada' });
+    res.status(200).json(historicalReservation);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Error al obtener la reserva historica' });
+  }
+};
+
+//Obtener todas las reservas historicas
+exports.getAllHistoricalPayments = async (_, res) => {
+  try {
+    const historicalPayments = await HistoricalPayment.find();
+    res.status(200).json(historicalPayments);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Error al encontrar los pagos historicos' });
+  }
+};
+
+//Obtener una reserva historica por ID
+exports.getHistoricalPaymentById = async (req, res) => {
+  try {
+    const historicalPayment = await HistoricalPayment.findById(req.params.id);
+    if (!historicalPaymen)
+      return res.status(404).json({ error: 'Pago historico no encontrado' });
+    res.status(200).json(historicalReservation);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Error al obtener el pago historico' });
+  }
+};
