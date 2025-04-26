166e78abb6f78ba0860e3000dcf687b12e7dffb9
Romina Rivera <rdrivera@uc.cl>
2025-04-25 22:07:34 -0400

    agregar lógica de paginación y manejo de errores en el hook usePayments

--- Front-end/src/Hooks/usePayments.jsx
+++ Front-end/src/Hooks/usePayments.jsx
@@ -2,21 +2,31 @@ import { useState, useEffect } from 'react';
 import API from '../Services/Api';
 const usePayments = () => {
   const [payments, setPayments] = useState([]);
+  const [pagination, setPagination] = useState({
+    currentPage: 1,
+    itemsPerPage: 10,
+    totalItems: 0,
+    totalPages: 0,
+  });
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  const fetchPayments = async (page = 1) => {
+    try {
+      setIsLoading(true);
+      const res = await API.get(`/payments?page=${page}&limit=10`);
+      setPayments(res.data.payments);
+      setPagination(res.data.pagination);
+    } catch (error) {
+      console.error('Error al obtener los pagos: ', error);
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   useEffect(() => {
-    const fetchPayments = async () => {
-      try {
-        const res = await API.get('/payments');
-        setPayments(res.data);
-      } catch (error) {
-        console.error('Error al obtener los pagos: ', error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
     fetchPayments();
   }, []);
-  return { payments, isLoading };
+  return { payments, pagination, isLoading, error, fetchPayments };
 };
 export default usePayments;
